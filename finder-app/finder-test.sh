ea92f282f9112bf982fb77c1ff225fae3e84ec87
Bojan Drvenica <bojan.dr@outlook.com>
2026-05-01 15:01:15 +0200

    feat: fixed unit-test and created finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -5,69 +5,26 @@
 set -e
 set -u
 
-NUMFILES=10
-WRITESTR=AELD_IS_FUN
-WRITEDIR=/tmp/aeld-data
+filesdir=""
+searchstr=""
 username=$(cat conf/username.txt)
 
-if [ $# -lt 3 ]
+if [ $# -lt 2 ]
 then
-	echo "Using default value ${WRITESTR} for string to write"
-	if [ $# -lt 1 ]
-	then
-		echo "Using default value ${NUMFILES} for number of files to write"
-	else
-		NUMFILES=$1
-	fi	
-else
-	NUMFILES=$1
-	WRITESTR=$2
-	WRITEDIR=/tmp/aeld-data/$3
-fi
-
-MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
-
-echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
-
-rm -rf "${WRITEDIR}"
-
-# create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
-
-if [ $assignment != 'assignment1' ]
-then
-	mkdir -p "$WRITEDIR"
-
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-	#The quotes signify that the entire string in WRITEDIR is a single string.
-	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
-fi
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
-
-for i in $( seq 1 $NUMFILES)
-do
-	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
-done
-
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
-
-# remove temporary directories
-rm -rf /tmp/aeld-data
-
-set +e
-echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
-if [ $? -eq 0 ]; then
-	echo "success"
-	exit 0
-else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+	echo "This script takes two input arguments. Not enough arguments provided."
 	exit 1
+else
+	filesdir=$1
+	searchstr=$2
 fi
+
+if [ -d "$filesdir" ]; then
+    echo "${filesdir} is a directory"
+fi
+
+X=$(find $filesdir -type f | wc -l)
+Y=$(grep -r "pattern" $filesdir | wc -l)
+
+MATCHSTR="The number of files are ${X} and the number of matching lines are ${Y}"
+
+echo $MATCHSTR
